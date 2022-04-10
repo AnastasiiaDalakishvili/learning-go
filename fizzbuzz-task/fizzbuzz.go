2f6d8be1f6408748da9fdd25147c7e9cca66cbb3
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2022-04-10 14:19:17 +0100

    pass test: add functionality -  15 modulus of 3 and 5 should return fizzbuzz

--- fizzbuzz-task/fizzbuzz.go
+++ fizzbuzz-task/fizzbuzz.go
@@ -17,10 +17,12 @@ func fizzbuzz(number int) string {
 	fizzbuzzResult := ""
 
 	for i := 1; i <= number; i++ {
-		if i%3 == 0 {
-			fizzbuzzResult = fizzbuzzResult + "fizz"
+		if i%3 == 0 && i%5 == 0 {
+			fizzbuzzResult = fizzbuzzResult + "fizzbuzz"
 		} else if i%5 == 0 {
 			fizzbuzzResult = fizzbuzzResult + "buzz"
+		} else if i%3 == 0 {
+			fizzbuzzResult = fizzbuzzResult + "fizz"
 		} else {
 			fizzbuzzResult = fizzbuzzResult + strconv.Itoa(i)
 		}
