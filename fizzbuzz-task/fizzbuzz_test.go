650809be8b7ab3a0051ea09b8a9d346cbd85ca46
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2022-04-10 12:36:45 +0100

    failed test: fizzbuzz function -  pass 1 expect 1

---
+++ fizzbuzz-task/fizzbuzz_test.go
@@ -0,0 +1,20 @@
+package main
+
+import "testing"
+
+/*
+	fizzbuzz(1)  should output 1
+	fizzbuzz(2)  should output 1 2
+	fizzbuzz(3)  should output 1 2 fizz
+	fizzbuzz(5) should output 1 2 fizz 4 buzz
+	fizzbuzz(15) should output 1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz
+*/
+
+func TestFizzbuzz(t *testing.T) {
+	got := Fizzbuzz(1)
+	want := 1
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
